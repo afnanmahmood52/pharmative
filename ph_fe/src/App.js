aeb25f4cd815410852d942c4d58f92084134834f
Afnan Mahmood <47634321+afnanmahmood52@users.noreply.github.com>
2021-05-14 00:01:45 -0700

    env variables corrected

--- ph_fe/src/App.js
+++ ph_fe/src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-2"></div>
-          <div className="col-sm-8">
-            <h1>App</h1>
-          </div>
-          <div className="col-sm-2"></div>
-        </div>
-
-      </div>
+      <Header/>
 
 
       
--- ph_fe/src/Components/Header/Header.jsx
+++ ph_fe/src/Components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Header.css'
+import PharmativeLogo from "../../Images/PHARMATIVE.png"
+
 
 export default function Header() {
     return (
-        <div>
+        <div className="Navbar">
+            <div className="logo-section">
+                <img src={PharmativeLogo} alt="pharmative logo"/>
+            </div>
             
+            <div className="links-section">
+                
+            </div>
+
         </div>
     )
 }
